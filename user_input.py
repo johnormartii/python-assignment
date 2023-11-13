cc7da7c789b9db8cf79b033051503553e49e8a8b
john ormartii <johnormartii@gmail.com>
2023-11-13 20:39:08 +0300

    this is my python fast assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#name
+name = input("What is your name? ")
+#age
+age = input("How old are you? ")
+#location
+location = input("Where do you live? ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
